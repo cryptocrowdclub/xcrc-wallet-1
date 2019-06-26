c28f4b67995ae72f1009be9f13e87b46ed33a6a0
CRyptoCrowd.city <44811053+cryptocrowdclub@users.noreply.github.com>
2019-06-26 06:00:57 +0300

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -80,7 +80,6 @@ $(package)_config_opts += -silent
 $(package)_config_opts += -v
 $(package)_config_opts += -no-feature-printer
 $(package)_config_opts += -no-feature-printdialog
-$(package)_config_opts += -fast
 
 ifneq ($(build_os),darwin)
 $(package)_config_opts_darwin = -xplatform macx-clang-linux
